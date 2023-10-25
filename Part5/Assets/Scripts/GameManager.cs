2cc46393f87a0aafeae0c3ba0b17fa5bafa1f6db
DenysRudenko <heartbreathing19951014@gmail.com>
2023-10-25 22:42:47 +0100

    Created a method that start the game after button clicked.

--- Part5/Assets/Scripts/GameManager.cs
+++ Part5/Assets/Scripts/GameManager.cs
@@ -19,10 +19,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {   
-        isGameActive = true;
-        score = 0;
-        StartCoroutine(SpawnTarget());
-        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -54,5 +51,12 @@ public class GameManager : MonoBehaviour
 
     public void RestartGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame() {
+        isGameActive = true;
+        score = 0;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
     }
 }
